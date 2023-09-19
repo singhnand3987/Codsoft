7c25efb023e65922127439db5c53e7eba21d015b
singhnand3987 <132788919+singhnand3987@users.noreply.github.com>
2023-09-19 18:22:22 +0530

    Add files via upload

---
+++ Codsoft/Task2/StudentGradeCalculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        double[] marks = new double[numSubjects];
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextDouble();
+        }
+
+        double totalMarks = 0;
+        for (double mark : marks) {
+            totalMarks += mark;
+        }
+
+        double averagePercentage = (totalMarks / (numSubjects * 100)) * 100;
+
+        String grade = calculateGrade(averagePercentage);
+
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    public static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
